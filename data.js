c55c239220749814d105cb4a9e55e6adef539fe0
Mahmud Ulashev <urgutmahmud@gmail.com>
2025-06-24 15:18:36 +0500

    Add project data and rendering logic for task management

---
+++ data.js
@@ -0,0 +1,59 @@
+
+
+let data = [ 
+    {
+        taskName: 'Research',
+        Estimate: '2d 4h',
+        SpentTime: '1d 2h',
+        Assignee: './assets/profile-pics/project-pic/project-pic-1.png',
+        Priority: 'man',
+        progress: 'In Progress',
+        progressBar: './assets/profile-pics/project-pic/project-pic-ransiz.svg'
+
+    },
+    {
+        taskName: 'Mind Map',
+        Estimate: '2d 4h',
+        SpentTime: '1d 2h',
+        Assignee: './assets/profile-pics/project-pic/project-pic-1.png',
+        Priority: 'man',
+        progress: 'Done',
+        progressBar: './assets/profile-pics/project-pic/project-pic-ransiz.svg'
+
+    },
+    {
+        taskName: 'Mind Map',
+        Estimate: '2d 4h',
+        SpentTime: '1d 2h',
+        Assignee: './assets/profile-pics/project-pic/project-pic-1.png',
+        Priority: 'man',
+        progress: 'Done',
+        progressBar: './assets/profile-pics/project-pic/project-pic-ransiz.svg'
+
+    },
+    {
+        taskName: 'Mind Map',
+        Estimate: '2d 4h',
+        SpentTime: '1d 2h',
+        Assignee: './assets/profile-pics/project-pic/project-pic-1.png',
+        Priority: 'man',
+        progress: 'Done',
+        progressBar: './assets/profile-pics/project-pic/project-pic-ransiz.svg'
+
+    },
+    {
+        taskName: 'Mind Map',
+        Estimate: '2d 4h',
+        SpentTime: '1d 2h',
+        Assignee: './assets/profile-pics/project-pic/project-pic-1.png',
+        Priority: 'man',
+        progress: 'Done',
+        progressBar: './assets/profile-pics/project-pic/project-pic-ransiz.svg'
+
+    }
+];
+
+
+export default data;
+
+
