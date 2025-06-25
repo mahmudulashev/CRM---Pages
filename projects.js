122d9e6a6df7a1d326cabae71c85327e35bd0d60
Mahmud Ulashev <urgutmahmud@gmail.com>
2025-06-25 12:06:15 +0500

    Add UUIDs to tasks and update priority color classes in project display

--- projects.js
+++ projects.js
@@ -1,43 +1,49 @@
-
 import data from './data.js';
-let people = document.getElementById('people')
+let people = document.getElementById('people');
 
-function qoshish(data) {
-    people.innerHTML += ` <div class="people-div">
-                        <div class="orab-turuvchi">
-                            <div class="name">
-                                <p>Task Name</p>
-                                <h4>${data.taskName}</h4>
-                            </div>
-                            <div class="data">
-                                <p>Estimate</p>
-                                <h4>${data.Estimate}</h4>
-                            </div>
-                            <div class="time">
-                                <p>Spent Time</p>
-                                <h4>${data.SpentTime}</h4>
-                            </div>
-                            <div class="text-img">
-                                <p>Assignee</p>
-                                <img style="border-radius:50px; margin: 5px;" width="30px"
-                                    src="${data.Assignee}" alt="bola rasmi">
+function qoshish(task) {
+    let priorityClass = '';
+    if (task.Priority === 'Medium') {
+        priorityClass = 'sariq';
+    } else if (task.Priority === 'Low') {
+        priorityClass = 'yashil';
+    }
 
-
-                            </div>
-                            <div class="level">
-                                <p>Priority</p>
-                                <h4>${data.Priority}</h4>
-                            </div>
-                            <div class="tugma-div">
-                                <button>${data.progress}</button>
-                            </div>
-                            <div class="aynala">
-                                 <img src=" ${data.progressBar}" alt="">
-
-                            </div>
-                        </div>
-                    </div>`
+    people.innerHTML += `
+        <div class="people-div">
+            <div class="orab-turuvchi">
+                <div class="name">
+                    <p>Task Name</p>
+                    <h4>${task.taskName}</h4>
+                </div>
+                <div class="data">
+                    <p>Estimate</p>
+                    <h4>${task.Estimate}</h4>
+                </div>
+                <div class="time">
+                    <p>Spent Time</p>
+                    <h4>${task.SpentTime}</h4>
+                </div>
+                <div class="text-img">
+                    <p>Assignee</p>
+                    <img style="border-radius:50px; margin: 5px;" width="30px"
+                        src="${task.Assignee}" alt="bola rasmi">
+                </div>
+                <div class="level">
+                    <p>Priority</p>
+                    <h4 class="priority ${priorityClass}">${task.Priority}</h4>
+                </div>
+                <div class="tugma-div">
+                    <button>${task.progress}</button>
+                </div>
+                <div class="aynala">
+                    <img src="${task.progressBar}" alt="">
+                </div>
+            </div>
+        </div>`;
 }
+
 for (let i = 0; i < data.length; i++) {
     qoshish(data[i]);
 }
+
