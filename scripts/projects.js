dfe960f0de25ab8a0c7302db4f968ae6a5bb2072
Mahmud Ulashev <urgutmahmud@gmail.com>
2025-06-27 15:31:18 +0500

    Refactor project display: update HTML structure and enhance project rendering logic

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,25 @@
 import { projects } from './data.js';
 let people = document.getElementById('people');
+let container = document.querySelector('.container');
+
+function projectQoshish(project, index) {
+    return `<div class="current-div1" data-index="${index}">
+                <p>${project.pn}</p>
+                <h4>${project.name}</h4>
+                <a href="#">View details
+                    <svg width="6" height="10" viewBox="0 0 6 10" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M0.292893 0.292893C0.653377 -0.0675907 1.22061 -0.0953203 1.6129 0.209705L1.70711 0.292893L5.70711 4.29289C6.06759 4.65338 6.09532 5.22061 5.7903 5.6129L5.70711 5.70711L1.70711 9.70711C1.31658 10.0976 0.683418 10.0976 0.292893 9.70711C-0.0675907 9.34662 -0.0953203 8.77939 0.209705 8.3871L0.292893 8.29289L3.585 5L0.292893 1.70711C-0.0675907 1.34662 -0.0953203 0.779392 0.209705 0.387101L0.292893 0.292893Z"
+                    fill="#3F8CFF" />
+                    </svg>
+                </a>
+            </div>`;
+}
+
+for (let i = 0; i < projects.length; i++) {
+    container.innerHTML += projectQoshish(projects[i], i);   
+}
 
 
 function qoshish(task) {
@@ -11,19 +31,19 @@ function qoshish(task) {
     }
 
     let progressClass = '';
-     if (task.progress === 'Done') {
-       progressClass = 'done';
+    if (task.progress === 'Done') {
+        progressClass = 'done';
     } else if (task.progress === 'In Progress') {
         progressClass = 'in-progress';
     } else if (task.progress === 'To Do') {
         progressClass = 'to-do'
-    } else if (task.progress === 'In Review'){
+    } else if (task.progress === 'In Review') {
         progressClass = 'in-review'
     }
 
 
-    
-      let peopleDiv =  `<div class="people-div">
+
+    let peopleDiv = `<div class="people-div">
             <div class="orab-turuvchi">
                 <div class="name">
                     <p>Task Name</p>
@@ -55,7 +75,7 @@ function qoshish(task) {
             </div>
         </div>`;
 
-        return peopleDiv;
+    return peopleDiv;
 }
 
 let html = '';
